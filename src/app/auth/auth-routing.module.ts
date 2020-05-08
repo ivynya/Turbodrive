476bbf12c2b9cd3f4b9a7931dc45c271fda7ba26
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-05-08 00:38:53 -0700

    Authorization token
    Get auth token from URL. I realize that the client secret is not safe, it's probably fine for now.

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth.component';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: 'oauthcallback',
+    component: AuthComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule {}
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { google } from 'googleapis';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+  url: string;
+  secrets: Object;
 
-  constructor() { }
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+    this.http.get("http://localhost:4200/assets/appsettings.json")
+      .subscribe(data => { 
+        this.secrets = data; 
+
+        const oauth2Client = new google.auth.OAuth2(
+          this.secrets["googleClientId"],
+          this.secrets["googleClientSecret"],
+          "http://localhost:4200/oauthcallback"
+        );
+    
+        const scopes = [
+          'https://www.googleapis.com/auth/classroom.announcements.readonly',
+          'https://www.googleapis.com/auth/classroom.courses.readonly',
+          'https://www.googleapis.com/auth/classroom.coursework.me'
+        ];
+    
+        this.url = oauth2Client.generateAuthUrl({
+          // 'online' (default) or 'offline' (gets refresh_token)
+          access_type: 'offline',
+          scope: scopes
+        });
+      });
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log(params["code"]);
+    });
   }
 
 }
