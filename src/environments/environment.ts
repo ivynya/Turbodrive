a84fb2835a7da8e60e632331160ef83f46be6c67
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-05-20 00:07:47 -0700

    Contained scrollbars
    - Scrollbars are now contained within the loaded sections of class assignment groups

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const AppConfig = {
+  production: false,
+  environment: 'LOCAL',
+  googleClientId: '45992447498-512fhhgf9333ou279pbrmc5bs582v393.apps.googleusercontent.com',
+  googleClientSecret: '',
+  googleCallbackUrl: 'http://localhost:4200/oauthcallback'
+};
