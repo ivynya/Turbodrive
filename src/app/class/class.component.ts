5b935ffe0bedb538ea50c87c1e88bc148e2a4ac7
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-05-21 21:47:08 -0700

    Navbar & tooltip behaviour change, load class pages
    - Navbar now shows active link
    - Tooltip does not show on focus, custom focus is moved entirely to the element itself
    - Class pages now show the class name

--- src/app/class/class.component.ts
+++ src/app/class/class.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { classroom_v1 } from 'googleapis';
+import * as Store from 'electron-store';
 
 @Component({
   selector: 'app-class',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./class.component.scss']
 })
 export class ClassComponent implements OnInit {
+  course: classroom_v1.Schema$Course;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private activatedRoute: ActivatedRoute) { 
   }
 
+  ngOnInit(): void {
+    const store = new Store();
+    const courses = store.get("courses");
+
+    this.activatedRoute.params.subscribe(params => {
+      this.course = courses.filter((c: classroom_v1.Schema$Course) => {
+        return c.id === this.activatedRoute.snapshot.params.id;
+      })[0];
+      console.log(this.course);
+    });
+  }
 }
