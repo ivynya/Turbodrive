4e16ebdff70bf42c23ffc2a36fcc91a31d1493a1
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-05-28 21:14:45 -0700

    Add default data for class component test
    - Added data for class component test

--- src/app/class/class.component.spec.ts
+++ src/app/class/class.component.spec.ts
@@ -3,9 +3,42 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ClassComponent } from './class.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { classroom_v1 } from 'googleapis';
+import * as Store from 'electron-store';
+
 describe('ClassComponent', () => {
   let component: ClassComponent;
   let fixture: ComponentFixture<ClassComponent>;
+  const store: Store = new Store();
+
+  // Spec according to courseData and courses schemas
+  const defaultData: { 
+    courses: classroom_v1.Schema$Course[], 
+    courseData: {
+      [id: string]: {
+        announcements: classroom_v1.Schema$Announcement[],
+        assignments: classroom_v1.Schema$CourseWork[]
+      }
+    }
+  } = { 
+    "courses": [{ "id": "123", 
+      "name": "Class", "ownerId": "", "creationTime": "",
+      "updateTime": "", "enrollmentCode": "", "courseState": "",
+      "alternateLink": "", "teacherGroupEmail": "",
+      "courseGroupEmail": "", "guardiansEnabled": false, "calendarId": ""}],
+    "courseData": { "123": {
+			"announcements": [
+				{ "courseId": "123", "id": "456", "text": "", "materials": [],
+					"state": "PUBLISHED", "alternateLink": "", "creationTime": "",
+					"updateTime": "", "creatorUserId": "" }],
+			"assignments": [
+				{ "courseId": "123", "id": "789", "title": "", "materials": [],
+					"state": "PUBLISHED", "alternateLink": "", "creationTime": "",
+					"updateTime": "", "dueDate": { "year": 2020, "month": 5, "day": 30 },
+					"dueTime": { "hours": 6, "minutes": 59 }, "maxPoints": 100,
+					"workType": "ASSIGNMENT", "submissionModificationMode": "MODIFIABLE_UNTIL_TURNED_IN",
+					"creatorUserId": ""}]}}
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,6 +49,8 @@ describe('ClassComponent', () => {
   }));
 
   beforeEach(() => {
+    store.store = defaultData;
+
     fixture = TestBed.createComponent(ClassComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
