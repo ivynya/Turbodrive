26522b768cb6c816bf67e4eacb4f389759e10d33
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-06-21 02:32:48 -0700

    Late assignments feed

--- src/app/class/class.component.ts
+++ src/app/class/class.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService, StorageService } from '../core/services';
 import { Turbo$Announcement, Turbo$CourseWork, Turbo$Course } from '../core/schemas';
+import { WriteKeyExpr } from '@angular/compiler';
 
 @Component({
   selector: 'app-class',
@@ -11,6 +12,7 @@ import { Turbo$Announcement, Turbo$CourseWork, Turbo$Course } from '../core/sche
 export class ClassComponent implements OnInit {
   course: Turbo$Course;
   feed: Array<Turbo$Announcement|Turbo$CourseWork> = [];
+  late: Array<Turbo$CourseWork> = [];
 
   constructor(private activatedRoute: ActivatedRoute,
               private data: DataService,
@@ -18,29 +20,42 @@ export class ClassComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.storage.has("courses")) {
-      const courses = this.storage.get("courses");
+      const courses: Turbo$Course[] = this.storage.get("courses");
 
       this.activatedRoute.params.subscribe(params => {
-        courses.forEach((course: Turbo$Course) => {
-          if (course.id === params.id) {
-            this.course = course;
+        this.course = courses.find((c) => c.id === params.id);
 
-            this.data.subscribeCourseData(this.course.id, (data) => {
-              // Reset feed
-              this.feed = [];
-              // Concatenates all items, sorts by updateTime descending
-              this.feed = this.feed.concat(data.announcements);
-              this.feed = this.feed.concat(data.assignments);
-              this.feed = this.feed.sort((a, b) => { 
-                return (a.updateTime < b.updateTime) ? 1 : -1
-              });
-            }, true);
+        this.data.subscribeCourseData(this.course.id, (data) => {
+          // Reset feed
+          this.feed = [];
+          // Concatenates all items, sorts by updateTime descending
+          this.feed = this.feed.concat(data.announcements);
+          this.feed = this.feed.concat(data.assignments);
+          this.feed = this.feed.sort((a, b) => { 
+            return (a.updateTime < b.updateTime) ? 1 : -1
+          });
 
-            this.data.tryMarkCourseRead(course.id);
-          }
-        });
+          this.generateLateFeed(data.assignments);
+        }, true);
+
+        this.data.tryMarkCourseRead(this.course.id);
       });
     }
+  }
+
+  generateLateFeed(assignments: Turbo$CourseWork[]) {
+    // Generate late work feed
+    this.late = assignments.filter((work) => {
+      if (!work.dueDate) return false;
+
+      const wd = new Date();
+      wd.setUTCFullYear(work.dueDate.year, work.dueDate.month, work.dueDate.day);
+      wd.setUTCHours(work.dueTime.hours);
+      wd.setUTCMinutes(work.dueTime.minutes ?? 0);
+      wd.setUTCSeconds(0);
+
+      return (wd < new Date() && !work.read);
+    });
   }
   
   markAsRead(courseId: string, type: string, id: string): void {
