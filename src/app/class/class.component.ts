c1337c4d1d3562950078b247be8cd94d4f21bdf9
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-06-21 19:35:16 -0700

    Class component sortable feed

--- src/app/class/class.component.ts
+++ src/app/class/class.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService, StorageService } from '../core/services';
-import { Turbo$Announcement, Turbo$CourseWork, Turbo$Course } from '../core/schemas';
+import { Turbo$Announcement, Turbo$CourseWork, Turbo$Course, Turbo$CourseData } from '../core/schemas';
+
+enum FeedType {
+  Stream,
+  Announcements,
+  Assignments,
+  Topics
+}
 
 @Component({
   selector: 'app-class',
@@ -10,8 +17,13 @@ import { Turbo$Announcement, Turbo$CourseWork, Turbo$Course } from '../core/sche
 })
 export class ClassComponent implements OnInit {
   course: Turbo$Course;
+
+  courseData: Turbo$CourseData;
   feed: Array<Turbo$Announcement|Turbo$CourseWork> = [];
-  late: Array<Turbo$CourseWork> = [];
+  feedType: FeedType = FeedType.Stream;
+
+  upcoming: Turbo$CourseWork[] = [];
+  late: Turbo$CourseWork[] = [];
 
   constructor(private activatedRoute: ActivatedRoute,
               private data: DataService,
@@ -27,21 +39,79 @@ export class ClassComponent implements OnInit {
         if (!this.course) return;
 
         this.data.subscribeCourseData(this.course.id, (data) => {
-          // Reset feed
-          this.feed = [];
-          // Concatenates all items, sorts by updateTime descending
-          this.feed = this.feed.concat(data.announcements);
-          this.feed = this.feed.concat(data.assignments);
-          this.feed = this.feed.sort((a, b) => { 
-            return (a.updateTime < b.updateTime) ? 1 : -1
-          });
+          this.courseData = data;
 
+          this.updateFeed(data);
           this.generateLateFeed(data.assignments);
         }, true);
 
         this.data.tryMarkCourseRead(this.course.id);
       });
     }
+  }
+
+  setFeedType(type: number): void {
+    document.getElementById(`ft-${this.feedType}`).classList.remove("active");
+    document.getElementById(`ft-${type}`).classList.add("active");
+
+    this.feedType = type;
+    this.updateFeed(this.courseData);
+  }
+
+  // Update main feed based on current feed type
+  updateFeed(data: Turbo$CourseData): void {
+    // Reset feed
+    this.feed = [];
+
+    // Call appropriate function for each type
+    switch (this.feedType) {
+      case FeedType.Stream:
+        this.generateStream(data);
+        break;
+      case FeedType.Announcements:
+        this.generateAnnouncements(data.announcements);
+        break;
+      case FeedType.Assignments:
+        this.generateAssignments(data.assignments);
+        break;
+      case FeedType.Topics:
+        break;
+    }
+  }
+
+  // Generates a feed with announcements only
+  generateAnnouncements(announcements: Turbo$Announcement[]): void {
+    this.feed = announcements;
+  }
+  
+  // Generates a feed with assignments only
+  generateAssignments(assignments: Turbo$CourseWork[]): void {
+    this.feed = assignments;
+  }
+
+  // Generates a feed with all announcements and assignments
+  generateStream(data: Turbo$CourseData): void {
+    // Concatenates all items, sorts by updateTime descending
+    this.feed = this.feed.concat(data.announcements);
+    this.feed = this.feed.concat(data.assignments);
+    this.feed = this.feed.sort((a, b) => { 
+      return (a.updateTime < b.updateTime) ? 1 : -1
+    });
+  }
+
+  generateUpcomingFeed(assignments: Turbo$CourseWork[]): void {
+    // Generate late work feed
+    this.late = assignments.filter((work) => {
+      if (!work.dueDate) return false;
+
+      const wd = new Date();
+      wd.setUTCFullYear(work.dueDate.year, work.dueDate.month, work.dueDate.day);
+      wd.setUTCHours(work.dueTime.hours);
+      wd.setUTCMinutes(work.dueTime.minutes ?? 0);
+      wd.setUTCSeconds(0);
+
+      return (wd > new Date() && !work.read);
+    });
   }
 
   generateLateFeed(assignments: Turbo$CourseWork[]): void {
