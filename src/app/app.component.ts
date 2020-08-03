0b500cdc9bd03f6a3d1eb57c10e19ce3912eb2a0
Kyle <sdbagel@gmail.com>
2020-08-02 23:00:17 -0700

    Add window control handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,5 +51,49 @@ export class AppComponent implements OnInit {
     else {
       this.router.navigateByUrl("/auth");
     }
+        
+    const win = this.electron.remote.getCurrentWindow();
+    window.onbeforeunload = (): void => {
+      /* If window is reloaded, remove win event listeners
+      (DOM element listeners get auto garbage collected but not
+      Electron win listeners as the win is not dereferenced unless closed) */
+      win.removeAllListeners();
+    }
+
+    this.handleWindowControls();
+  }
+
+  handleWindowControls(): void {
+    const win = this.electron.remote.getCurrentWindow();
+
+    // Make minimise/maximise/restore/close buttons work when they are clicked
+    document.getElementById('min-button').addEventListener("click", () => {
+      win.minimize();
+    });
+
+    document.getElementById('max-button').addEventListener("click", () => {
+      win.maximize();
+    });
+
+    document.getElementById('restore-button').addEventListener("click", () => {
+      win.unmaximize();
+    });
+
+    document.getElementById('close-button').addEventListener("click", () => {
+      win.close();
+    });
+
+    function toggleMaxRestoreButtons(): void {
+      if (win.isMaximized()) {
+        document.body.classList.add('maximized');
+      } else {
+        document.body.classList.remove('maximized');
+      }
+    }
+
+    // Toggle maximise/restore buttons when maximisation/unmaximisation occurs
+    toggleMaxRestoreButtons();
+    win.on('maximize', toggleMaxRestoreButtons);
+    win.on('unmaximize', toggleMaxRestoreButtons);
   }
 }
