08035d15e9b98dbf6749b103300fd9e009fa251b
Kyle <sdbagel@gmail.com>
2020-10-31 23:42:49 -0700

    Upgrade to Electron 10.1.3

--- main.ts
+++ main.ts
@@ -16,6 +16,7 @@ function createWindow(): BrowserWindow {
     webPreferences: {
       nodeIntegration: true,
       allowRunningInsecureContent: (serve) ? true : false,
+      enableRemoteModule: true
     },
   });
 
